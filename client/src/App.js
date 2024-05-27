896dabeb3022904b459a53e34cd14eec56612631
seif <mohamedahmedseifofficial@gmail.com>
2024-05-27 19:17:12 +0300

    Testing on viewer

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,10 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import  LogIn  from './pages/LogIn';
 import Dashboard from './pages/Dashboard';
-import BookReader from './pages/BookReader';
-import Reader2 from './pages/Reader2'
 import Logout from './pages/Logout';
 import BookPublic from './pages/BookPublic';
-import Viewer from './pages/Viewer'
+import Viewer from './pages/Viewer';
+import ViewerTest from 'pages/ViewerTest';
 
 function App() {
   let stat =  localStorage.getItem("IsLoggedIn")
@@ -29,10 +28,9 @@ function App() {
             <Route path='/logIn' element={<LogIn/>}></Route>
             <Route path='/logout' element={<Logout/>}></Route>
             <Route path='/dashboard' element={<Dashboard/>}></Route>
-            <Route path='/BookReader' element={<BookReader/>}></Route>
-            <Route path='/Reader2' element={<Reader2/>}></Route>
             <Route path='/bookPublic' element={<BookPublic/>}></Route>
             <Route path='/viewer' element={<Viewer/>}></Route>
+            <Route path='/viewerTest' element={<ViewerTest/>}></Route>
           </Routes>
       </div>
   );
